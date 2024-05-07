2dc17471fbd8d6683ee7ecad3c78fdaff665fabf
Suresh Subedi <sureshsubedi3994@gmail.com>
2024-05-07 12:48:33 +0545

    first commit

---
+++ seconds.java
@@ -0,0 +1,17 @@
+public class seconds {
+
+    int a = 10;
+    String b = "Suresh";
+
+    void show() {
+        System.out.println(a + " " + b);
+    }
+
+    class test {
+        public static void main(String[] args) {
+            seconds r = new seconds();
+            r.show();
+        }
+    }
+
+}
