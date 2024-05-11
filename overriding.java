ba1f63bd16e5862c761616084cbcd6ab04cb2174
Suresh Subedi <sureshsubedi3994@gmail.com>
2024-05-11 16:25:13 +0545

    2nd commit

---
+++ overriding.java
@@ -0,0 +1,21 @@
+class Animal {
+    void eat() {
+        System.out.println("eat(), method of base class");
+        System.out.println("Eating");
+    }
+
+    class Dog extends Animal {
+        void eat() {
+            System.out.println("Eat method of derived class");
+            System.out.println("Dog is eating");
+        }
+    }
+    class methodOverloading{
+        public static void main(String[] args) {
+            Dog d1Dog ;
+        }
+    }
+
+    
+    }
+
